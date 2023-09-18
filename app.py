cb7ae855db45de5f62506c2b9b62c33e087517c3
Gabrielatineo <131632382+Gabrielatineo@users.noreply.github.com>
2023-09-18 19:01:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return {"payload":"hack-1 finished"}
+
+
+
+
+
+if __name__ == "main":
+    app.run(debug=True)
